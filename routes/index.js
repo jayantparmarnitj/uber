61b0878946ef80da82702fb79b1bd3cab9393685
Sierra 4 <sierra4@OSX4Seirra.local>
2018-01-29 18:56:08 +0530

    Initing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
 module.exports = function(app) {
   var controller = require('../controllers/indexController');
 
-app.post('/driverGps',controller.find_driver);
+app.all('/driverGps',controller.find_driver);
 //app.post('/verifyOtp',controller.verify_otp);
 app.get('/',controller.list_card_data);
 
