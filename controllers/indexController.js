bd66b5725126ecab23e00fbfc2be48870b290921
Sierra 4 <sierra4@OSX4Seirra.local>
2018-01-29 18:08:23 +0530

    Initing

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -8,7 +8,7 @@ exports.list_card_data = function(req, res) {
 
 exports.find_driver = function(req, res) {
   try{
-    console.log(req.body);
+    console.log("longitude "+req.body.data.longitude);
     const longitude = req.body.data.longitude;
     const latitude = req.body.data.latitude;
     console.log("longitude: "+longitude);
