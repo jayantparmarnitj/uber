ae3164fb5203f84585d0a5512f8f17a31c98ba6c
Sierra 4 <sierra4@OSX4Seirra.local>
2018-02-01 13:47:10 +0530

    Initing

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
 
 app.all('/driverGps',controller.find_nearest_drivers);
 app.all('/allDrivers',controller.find_all_drivers);
+app.all('/driverSignup',controller.drivers_signup);
 // app.all('/userLogin',controller.user_login);
 // app.all('/driverLogin',controller.driver_login);
 //app.post('/verifyOtp',controller.verify_otp);
